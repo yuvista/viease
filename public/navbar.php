3d133b2c411f9d09e566f517a66b13ba63a44fd6
overtrue <anzhengchao@gmail.com>
2015-05-29 18:15:56 +0800

    更新

---
+++ public/navbar.php
@@ -0,0 +1,84 @@
+<div class="navbar">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+  <div class="">
+    <ul class="nav navbar-nav navbar-left">
+      <li class="active">
+        <a href="#">模块1</a>
+      </li>
+      <li>
+        <a href="#">模块2</a>
+      </li>
+      <li>
+        <a href="#">模块3</a>
+      </li>
+      <li>
+        <a href="#">模块4</a>
+      </li>
+      <li>
+        <a href="#">模块5</a>
+      </li>
+      <li>
+        <a href="#">模块6</a>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+          下拉 <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <a href="#">测试链接</a>
+          </li>
+          <li>
+            <a href="#">测试链接2</a>
+          </li>
+          <li>
+            <a href="#">其它的链接</a>
+          </li>
+          <li class="divider"></li>
+          <li class="dropdown-header">二级下拉</li>
+          <li>
+            <a href="#">演示链接</a>
+          </li>
+          <li>
+            <a href="#">更多链接</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li>
+        <a href="#">右导航</a>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          下拉 <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li>
+            <a href="#" >测试链接</a>
+          </li>
+          <li>
+            <a href="#">测试链接2</a>
+          </li>
+          <li>
+            <a href="#">其它的链接</a>
+          </li>
+          <li class="divider"></li>
+          <li class="dropdown-header">二级下拉</li>
+          <li>
+            <a href="#">演示链接</a>
+          </li>
+          <li>
+            <a href="#">更多链接</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
