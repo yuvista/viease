a6a4e7301301c39e9a034646a45c28f55fd7c769
overtrue <anzhengchao@gmail.com>
2015-06-03 10:38:42 +0800

    添加字段别名与验证属性

--- app/Http/Requests/Account/UpdateRequest.php
+++ app/Http/Requests/Account/UpdateRequest.php
@@ -1,14 +1,16 @@
-<?php 
+<?php
 
 namespace App\Http\Requests\Account;
 
 use App\Http\Requests\Request;
+use App\Models\Account;
 
 /**
  * Account UpdateRequest
  */
 class UpdateRequest extends Request
 {
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -19,7 +21,17 @@ class UpdateRequest extends Request
         return [
             'name'           => 'required',
             'original_id'    => 'required',
-            'wechat_account' => 'required'
+            'wechat_account' => 'required',
         ];
+    }
+
+    /**
+     * 验证不通过时返回的错误消息里的字段别名
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return Account::$attributes;
     }
 }
--- app/Models/Account.php
+++ app/Models/Account.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Account extends Model
 {
     use SoftDeletes;
+
+    /**
+     * 字段白名单
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'original_id',
+        'wechat_account',
+        'app_id',
+        'app_secret',
+        'account_type',
+    ];
+
+    /**
+     * 用于表单验证时的字段名称提示
+     *
+     * @var array
+     */
+    protected $aliases = [
+        'name'           => '公众号名称',
+        'original_id'    => '原始ID',
+        'wechat_account' => '微信账号',
+        'app_id'         => '应用ID',
+        'app_secret'     => '应用secret',
+        'account_type'   => '账户类型',
+    ];
 }
+
+
