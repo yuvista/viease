e75eb5212c1645d7fb63986e994c702a748d6541
songyanhui <songyanhui@adsage.com>
2015-07-01 02:13:15 +0800

    update database/migrations

---
+++ database/migrations/2015_06_08_020932_create_fan_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFanReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fan_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id')->nullable()->comment('所属公众号');
+			$table->string('openid',100)->nullable()->comment('OPENID');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fan_reports');
+    }
+}
