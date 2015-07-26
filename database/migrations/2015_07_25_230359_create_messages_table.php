b51b40032b66e2d0eee1e728aa11eec7e05cfdac
rongyouyuan <rongyouyuan@163.com>
2015-07-26 10:12:01 +0800

    提交消息部分

--- database/migrations/2015_07_25_230359_create_messages_table.php
+++ database/migrations/2015_07_25_230359_create_messages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesTables extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,22 @@ class CreateMessagesTables extends Migration
      */
     public function up()
     {
-         Schema::create('messages', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('account_id');
+            $table->integer('fans_id')->comment('粉丝id');
+            $table->enum('type', [
+                    'text',
+                    'image',
+                    'shortvideo',
+                    'location'
+                ])->comment('消息类型 text 文字 image 图片 shortvideo 短视频 location位置');
+            $table->timestamp('sent_at')->comment('消息发送时间');
+            $table->integer('resource_id')->comment('对应消息资源');
+            $table->integer('reply_id')->default(0)->nullable()->comment('消息回复id');
+            $table->timestamp('replied_at')->nullable()->comment('消息回复时间');
+            $table->json('content');
+            $table->string('msg_id',25)->comment('消息id');
             $table->timestamps();
         });
     }
--- database/migrations/2015_07_25_230849_create_message_resources_table.php
+++ database/migrations/2015_07_25_230849_create_message_resources_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesResourcesTable extends Migration
+class CreateMessageResourcesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,10 @@ class CreateMessagesResourcesTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('message_resources', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('account_id');
+            $table->string('resource_url',300)->nullable()->comment('资源访问地址');
             $table->timestamps();
         });
     }
